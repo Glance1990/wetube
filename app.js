5b9eae315f3872779b337e503e94e4947f446c97
Pavel <pavlo@nxte.nl>
2019-08-09 15:55:05 +0300

    starting point at 2.18 Search Conntroller section

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./controllers/middlewares";
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express()
 
-const handleHome = (req, res) => res.send('Hello Worldd!');
-
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
-app.use(helmet());
 
-app.get('/', handleHome);
+app.use(localsMiddleware);
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
